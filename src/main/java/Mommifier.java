7804a5d91d6580fd77333629ec3a8c0303bb81e0
culiutudousi <chenzhicheng1994@gmail.com>
2020-09-13 20:32:02 -0700

    should return original string when mommify given string that holds vowels less than 30 persent

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,5 @@
+public class Mommifier {
+    public String mommify(String string) {
+        return string;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MommifierTest {
+
+    public static final String VOWELS_LESS_THAN_30_PERCENT = "vowels less than 30 percent.";
+
+    @Test
+    void should_return_original_string_when_mommify_given_string_that_holds_vowels_less_than_30_percent() {
+        Mommifier mommifier = new Mommifier();
+        String mommifiedString = mommifier.mommify(VOWELS_LESS_THAN_30_PERCENT);
+        assertEquals(VOWELS_LESS_THAN_30_PERCENT, mommifiedString);
+    }
 }
