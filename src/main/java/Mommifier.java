2dcabb1a041e18f5377f5f88d5e4bff5469deedb
culiutudousi <chenzhicheng1994@gmail.com>
2020-09-14 04:55:47 -0700

    should return string with mommy before every vowel when mommify given string that holds separate vowels mores than 30 percent

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mommifier {
+
+    public static final String VOWELS = "aeiou";
+    public static final String MOMMY = "mommy";
+
     public String mommify(String string) {
-        return string;
+        if (!isVowelMoreThan(string, 0.3)) {
+            return string;
+        }
+        List<String> resultStringList = new ArrayList<>();
+        string.codePoints()
+                .mapToObj(c -> String.valueOf((char) c))
+                .forEachOrdered(s -> {
+                    if (VOWELS.contains(s.toLowerCase())) {
+                        resultStringList.add(MOMMY);
+                    }
+                    resultStringList.add(s);
+                });
+        return String.join("", resultStringList);
+    }
+
+    private Boolean isVowelMoreThan(String string, double percentage) {
+        long vowelNumber = string.codePoints()
+                .mapToObj(c -> String.valueOf((char) c))
+                .filter(s -> VOWELS.contains(s.toLowerCase()))
+                .count();
+        double vowelPercentage = (double) vowelNumber / string.length();
+        if (vowelPercentage > percentage) {
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -5,6 +5,8 @@ public class MommifierTest {
 
     public static final String VOWELS_LESS_THAN_30_PERCENT = "ae23456789";
     public static final String VOWELS_EQUALS_30_PERCENT = "aei3456789";
+    public static final String SEPARATE_VOWELS_MORE_THAN_30_PERCENT = "a1e3i5o6u789";
+    public static final String MOMMIFIED_SEPARATE_VOWELS_MORE_THAN_30_PERCENT = "mommya1mommye3mommyi5mommyo6mommyu789";
 
     @Test
     void should_return_original_string_when_mommify_given_string_that_holds_vowels_less_than_30_percent() {
@@ -18,5 +20,12 @@ public class MommifierTest {
         Mommifier mommifier = new Mommifier();
         String mommifiedString = mommifier.mommify(VOWELS_EQUALS_30_PERCENT);
         assertEquals(VOWELS_EQUALS_30_PERCENT, mommifiedString);
+    }
+
+    @Test
+    void should_return_string_with_mommy_before_every_vowel_when_mommify_given_string_that_holds_separate_vowels_more_than_30_percent() {
+        Mommifier mommifier = new Mommifier();
+        String mommifiedString = mommifier.mommify(SEPARATE_VOWELS_MORE_THAN_30_PERCENT);
+        assertEquals(MOMMIFIED_SEPARATE_VOWELS_MORE_THAN_30_PERCENT, mommifiedString);
     }
 }
